88dbdb3bf1ad8be01830d1418510fc90809d3bc8
sachinss2511 <sachin251194@gmail.com>
2022-04-15 15:26:46 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,31 @@
+// console.dir(document);
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// //document.title = 123
+// console.log(document.head)
+console.log(document.images)
+console.log(document.getElementById('hi'))
+console.log(document.getElementById('header-title'))
+var headerTitle = document.getElementById('header-title')
+// headerTitle.textContent = 'sahfshdk'
+// headerTitle.innerText  = 'sachin'
+console.log(headerTitle.textContent)
+console.log(headerTitle.innerText)
+headerTitle.innerHTML = '<h3>Hello</h3>'
+headerTitle.style.borderBottom='solid 3px black'
+var header =  document.getElementById('main-header')
+header.style.borderBottom = 'solid 3px black'
+
+
+//getting elements by class name
+var items = document.getElementsByClassName('list-group-item')
+console.log(items[1])
+items[1].textContent = 'Hello2'
+items[1].style.fontWeight = 'bold'
+items[1].style.backgroundColor = 'yellow'
+
+//coloring whole background of class
+for(var i = 0 ; i<items.length; i++){
+    items[i].style.backgroundColor='#f4f4f4'
+}
