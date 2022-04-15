17227835639700966e52a0101264260bfe7403eb
sachinss2511 <sachin251194@gmail.com>
2022-04-15 16:02:56 +0530

    task 4 of dom

--- dom.js
+++ dom.js
@@ -29,3 +29,8 @@ items[1].style.backgroundColor = 'yellow'
 for(var i = 0 ; i<items.length; i++){
     items[i].style.backgroundColor='#f4f4f4'
 }
+for (var i=0; i<items.length;i++){
+  items[i].style.fontWeight='bold'
+
+
+}
